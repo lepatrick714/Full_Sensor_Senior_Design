88e24242b032ff02484d1be66cfeaecf1a7c6150
lepatrick714 <leduylamc@gmail.com>
2017-07-27 12:38:52 -0700

    Header file

---
+++ headers/ZConstants.h
@@ -0,0 +1,38 @@
+#ifndef ZCONSTANTS_H 
+#define ZCONSTANTS_H
+
+const double TC1 = 190.6;
+const double TC2 = 305.3;
+const double TC3 = 369.8;
+const double TC4 = 425.1;
+const double TC5 = 126.2;
+const double TC6 = 304.2;
+
+
+
+const double zc1 = 0.286;
+const double zc2 = 0.279;
+const double zc3 = 0.276;
+const double zc4 = 0.274;
+const double zc5 = 0.289;
+const double zc6 = 0.274;
+
+
+
+const double vc1 = 98.6;
+const double vc2 = 145.5;
+const double vc3 = 200;
+const double vc4 = 255;
+const double vc5 = 89.2;
+const double vc6 = 94;
+
+
+
+
+const double pc1 = 45.99;
+const double pc2 = 48.72;
+const double pc3 = 42.48;
+const double pc4 = 37.96;
+const double pc5 = 34;
+const double pc6 = 73.83;
+#endif // FUEL_SENSOR_CONSTANTS
